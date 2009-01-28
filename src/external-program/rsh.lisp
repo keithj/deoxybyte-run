47a3deefbde9fc91383cc27a42b38be008102432
Keith James <keith@zen.wtgc.org>
2009-01-28 00:35:22 +0000

    Refactored the rsh functions to be more friendly.

--- src/external-program/rsh.lisp
+++ src/external-program/rsh.lisp
@@ -17,14 +17,21 @@
 
 (in-package :cl-io-utilities)
 
-(defparameter *rsh-executable* "rsh" "The rsh executable name.")
-(defparameter *ls-executable* "ls" "The ls executable name.")
-(defparameter *mkdir-executable* "mkdir" "The mkdir executable name.")
+(declaim (type pathname *remote-pathname-defaults*))
+(defparameter *remote-pathname-defaults* (pathname "/")
+  "The defaults used to fill in remote pathnames.")
+
+(defparameter *rsh-executable* "rsh"
+  "The rsh executable name.")
+(defparameter *ls-executable* "ls"
+  "The ls executable name.")
+(defparameter *mkdir-executable* "mkdir"
+  "The mkdir executable name.")
 
 (defparameter *wait-for-rsh-process*
-  #+sbcl t
-  #+lispworks nil
-  #-(or sbcl lispworks) t
+  #+:sbcl t
+  #+:lispworks nil
+  #-(or :sbcl :lispworks) t
   "Indicates whether Lisp program should wait for the rsh process, or
   not.")
 
@@ -44,7 +51,7 @@
 
 (defun rsh-exec (host &rest command)
   "Executes COMMAND on HOST using rsh/ssh and returns an instance of
-defclass rsh . If the command returns a non-zero exit code, a
+{defclass rsh} . If the command returns a non-zero exit code, a
 NON-ZERO-EXIT-ERROR error is raised."
   (let ((rsh (make-instance 'rsh :program *rsh-executable*
                                  :host host :args (cons host command)
@@ -55,75 +62,80 @@ NON-ZERO-EXIT-ERROR error is raised."
         rsh
       (error 'non-zero-exit-error :program rsh))))
 
-(defun rsh-list-files (host pathspec &optional (ignore-backups t))
+(defun merge-remote-pathnames (pathname
+                               &optional (defaults *remote-pathname-defaults*))
+  "Constructs a filled in pathname by completing the unspecified
+components from the defaults given by *remote-pathname-defaults*."
+  (merge-pathnames pathname defaults))
+
+(defun rsh-list-directory (host pathspec &key (ignore-backups t) filetype)
   "Returns a list of namestrings of files in directory PATHSPEC on
-HOST. If IGNORE-BACKUPS is T (the default) then tilda backup files are
-ignored."
-  (loop
-     for filename in (rsh-list host pathspec ignore-backups)
-     unless (ends-with-char-p filename #\/)
-     collect filename))
+HOST. PATHSPEC is first merged with remote pathname defaults. If
+IGNORE-BACKUPS is T (the default) then tilda backup files are
+ignored. The FILETYPE keyword may :file to return only
+files, :directory to return only directories or NIL to return both
+files and directories."
+  (let ((dir (merge-remote-pathnames (fad:pathname-as-directory pathspec))))
+    (loop
+       for filename in (rsh-ls host (namestring dir) ignore-backups)
+       if (ends-with-char-p filename #\/) collect filename into dirs
+       else collect filename into files
+       finally (return (ecase filetype
+                         (:file files)
+                         (:directory dirs)
+                         ((nil) (nconc dirs files)))))))
 
-(defun rsh-list-directories (host pathspec)
-  "Returns a list of namestrings of subdirectories in directory
-PATHSPEC on HOST."
-  (loop
-     for filename in (rsh-list host pathspec)
-     when (ends-with-char-p filename #\/)
-     collect (string-right-trim '(#\/) filename)))
+(defun rsh-file-exists-p (host filespec)
+  "Returns T if file FILESPEC exists on HOST, or NIL
+otherwise. FILESPEC is first merged with remote pathname defaults."
+  (let ((file (merge-remote-pathnames (fad:pathname-as-file filespec))))
+    (multiple-value-bind (ps fs)
+        (right-trim-path file)
+      (rsh-pathname-exists-p host ps fs :filetype :file))))
 
-(defun rsh-file-exists-p (host pathspec filespec)
-  "Returns T if file FILESPEC exists in directory PATHSPEC on HOST, or
-NIL otherwise."
-  (rsh-pathname-exists-p host pathspec filespec #'rsh-list-files))
+(defun rsh-directory-exists-p (host pathspec)
+  "Returns T if PATHSPEC exists on HOST, or NIL otherwise. PATHSPEC is
+first merged with remote pathname defaults."
+  (let ((dir (merge-remote-pathnames (fad:pathname-as-directory pathspec))))
+    (multiple-value-bind (ps1 ps2)
+        (right-trim-path dir)
+      (rsh-pathname-exists-p host ps1 ps2 :filetype :directory))))
 
-(defun rsh-directory-exists-p (host pathspec1 pathspec2)
-  "Returns T if subdirectory PATHSPEC1 exists in directory PATHSPEC2
-on HOST, or NIL otherwise."
-  (rsh-pathname-exists-p host pathspec1 pathspec2 #'rsh-list-directories))
+(defun rsh-files-exist-p (host &rest filespecs)
+  "Returns T if all FILESPECS exist in directory on HOST, or NIL
+otherwise. Each FILESPEC is first merged with remote pathname
+defaults."
+  (subsetp filespecs (rsh-list-directory host *remote-pathname-defaults*
+                                         :filetype :file) :test #'equal))
 
-(defun rsh-files-exist-p (host pathspec &rest filespecs)
-  "Returns T if all of the files FILESPECS exist in directory PATHSPEC
-on HOST, or NIL otherwise."
-  (apply #'rsh-pathnames-exist-p host pathspec #'rsh-list-files
-         filespecs))
-
-(defun rsh-directories-exist-p (host pathspec &rest pathspecs)
-   "Returns T if all of the subdirectories PATHSPECS exist in
-directory PATHSPEC on HOST, or NIL otherwise."
-  (apply #'rsh-pathnames-exist-p host pathspec #'rsh-list-directories
-         pathspecs))
+(defun rsh-directories-exist-p (host &rest pathspecs)
+  "Returns T if all PATHSPECS exist in directory on HOST, or NIL
+otherwise. Each PATHSPEC is first merged with remote pathname
+defaults."
+  (subsetp (mapcar (lambda (ps)
+                     (namestring (fad:pathname-as-directory ps))) pathspecs)
+           (rsh-list-directory host *remote-pathname-defaults*
+                               :filetype :directory) :test #'equal))
 
 (defun rsh-make-directory (host pathspec)
-  "Creates a new directory PATHSPEC on HOST and returns PATHSPEC."
-  (close-process (rsh-exec host *mkdir-executable* pathspec))
-  pathspec)
+  "Creates a new directory specified by PATHSPEC on HOST and returns
+PATHSPEC. PATHSPEC is first merged with remote pathname defaults."
+  (rsh-mkdir host pathspec))
 
 (defun rsh-ensure-directories-exist (host pathspec)
   "Ensures that PATHSPEC exists on HOST, creating addional parent
-directories as required."
-  (close-process (rsh-exec host *mkdir-executable* "-p" pathspec))
-  pathspec)
+directories as required.  PATHSPEC is first merged with remote
+pathname defaults."
+  (rsh-mkdir host pathspec "-p"))
 
-(defun rsh-pathname-exists-p (host pathspec1 pathspec2 rsh-list-fn)
-  "Returns T if PATHSPEC1 exists in directory PATHSPEC2 on HOST, using
-RSH-LIST-FN to find the candidates for PATHSPEC1."
-  (let ((pname (parse-namestring pathspec2)))
-    (some (lambda (namestring)
-            (pathname-match-p (parse-namestring namestring) pname))
-          (funcall rsh-list-fn host pathspec1))))
+(defun rsh-pathname-exists-p (host pathname1 pathname2 &key filetype)
+  "Returns T if PATHNAME1 exists in directory PATHNAME2 on HOST, using
+RSH-LIST-FN to find the candidates for PATHNAME1."
+  (some (lambda (pn)
+          (equal pn (namestring pathname2)))
+        (rsh-list-directory host pathname1 :filetype filetype)))
 
-(defun rsh-pathnames-exist-p (host pathspec rsh-list-fn &rest pathspecs)
-  "Returns T if all of PATHSPECS exists in directory PATHSPEC on HOST,
-using RSH-LIST-FN to find the candidates for PATHSPEC."
-  (let ((pnames (mapcar #'parse-namestring pathspecs))
-        (remote-pnames (mapcar #'parse-namestring
-                               (funcall rsh-list-fn host pathspec))))
-    (every (lambda (pname)
-             (member pname remote-pnames :test #'pathname-match-p))
-           pnames)))
-
-(defun rsh-list (host pathspec &optional (ignore-backups t))
+(defun rsh-ls (host pathspec &optional (ignore-backups t))
   "Returns a list of pathspecs in PATHSPEC on HOST. If IGNORE-BACKUPS
 is T (the default) then tilda backup files are ignored."
   (let ((rsh (apply #'rsh-exec host *ls-executable*
@@ -137,3 +149,27 @@ is T (the default) then tilda backup files are ignored."
             while line
             collect line)
       (close-process rsh))))
+
+(defun rsh-mkdir (host pathspec &rest mkdir-args)
+  "Executes mkdir to create a directory specified by PATHSPEC on HOST
+using mkdir arguments MKDIR-ARGS. Returns PATHSPEC."
+  (let ((dir (fad:pathname-as-directory pathspec)))
+    (cond ((absolute-pathname-p dir)
+           (close-process (apply #'rsh-exec host *mkdir-executable*
+                                 (append mkdir-args (list pathspec))))
+           pathspec)
+          (t
+           (error 'invalid-argument-error
+                  :params 'pathspec
+                  :args pathspec
+                  :text "the pathspec must be absolute, not relative")))))
+
+(defun right-trim-path (pathname)
+  "Returns two values, being the root of PATHNAME with the terminal
+file or directory component removed and the removed component itself."
+  (if (fad:directory-pathname-p pathname)
+      (let ((dir (pathname-directory (fad:pathname-as-directory pathname))))
+        (values (make-pathname :directory (butlast dir))
+                (make-pathname :directory (cons :relative (last dir)))))
+    (values (make-pathname :directory (pathname-directory pathname))
+            (make-pathname :directory '(:relative) :defaults pathname))))
